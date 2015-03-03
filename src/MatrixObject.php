630dd57ba526430b17ae63cc1e78ebb74f6dee42
Petr Grishin <petr.grishin@grishini.ru>
2015-03-03 23:36:10 +0300

    Реализация интерфейса для объекта матрицы

---
+++ src/MatrixObject.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author Petr Grishin <petr.grishin@grishini.ru>
+ */
+ 
+
+namespace PetrGrishin\MatrixObject;
+
+
+interface MatrixObject {
+
+    /**
+     * @param array|MatrixObject $data
+     */
+    public function setArray($data);
+    /**
+     * @return array
+     */
+    public function getArray();
+}
